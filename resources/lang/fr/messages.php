7e8fcb9b6644fbf78525c7713b080fab10bd0706
jordyloeuille <32570668+jordyloeuille@users.noreply.github.com>
2026-08-20 14:35:52 +0200

    feat: add French package translations

---
+++ resources/lang/fr/messages.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'step' => 'Étape :current sur :total',
+    'progress_label' => 'Progression du formulaire',
+    'progress_value' => 'Étape :current sur :total, :percent pour cent terminé',
+    'fields_for_step' => 'Champs de l’étape :step',
+    'select_placeholder' => 'Choisissez une option',
+    'review_title' => 'Vérifiez vos informations',
+    'previous' => 'Étape précédente',
+    'loading_previous' => 'Chargement de l’étape précédente',
+    'continue' => 'Continuer',
+    'loading_next' => 'Chargement de l’étape suivante',
+    'submit' => 'Envoyer',
+    'submitting' => 'Envoi du formulaire',
+];
