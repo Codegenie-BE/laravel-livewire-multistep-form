a71bf8f136abe4ad26650f033b093d454b078cf2
jordyloeuille <32570668+jordyloeuille@users.noreply.github.com>
2026-08-20 14:35:43 +0200

    feat: add nl translations

---
+++ resources/lang/nl/messages.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'step' => 'Stap :current van :total',
+    'progress_label' => 'Formuliervoortgang',
+    'progress_value' => 'Stap :current van :total, :percent procent voltooid',
+    'fields_for_step' => 'Velden voor stap :step',
+    'select_placeholder' => 'Kies een optie',
+    'review_title' => 'Controleer je gegevens',
+    'previous' => 'Vorige stap',
+    'loading_previous' => 'Vorige stap laden',
+    'continue' => 'Volgende',
+    'loading_next' => 'Volgende stap laden',
+    'submit' => 'Verzenden',
+    'submitting' => 'Formulier verzenden',
+];
