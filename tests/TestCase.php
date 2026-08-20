d8ab608a55ece9bb2582fc6278148129680f377a
jordyloeuille <32570668+jordyloeuille@users.noreply.github.com>
2026-08-20 13:37:17 +0200

    test: boot package with Orchestra Testbench

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,15 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Codegenie\LivewireMultistepForm\LivewireMultistepFormServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
-abstract class TestCase extends BaseTestCase
+abstract class TestCase extends Orchestra
 {
-    //
+    protected function getPackageProviders($app): array
+    {
+        return [
+            LivewireMultistepFormServiceProvider::class,
+        ];
+    }
 }
